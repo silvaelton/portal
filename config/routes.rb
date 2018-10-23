a00c48008b702e531bf9e55aafbb61fe63a27853
Cleuber <cleubercanamari11@gmail.com>
2018-10-23 10:40:05 -0300

    Inicio da parte interna de acoes urbanas

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :posts, path: 'postagens'
   resources :actions, path: 'acoes-urbanas'
+  resources :social_events, path: 'eventos-sociais'
 end
