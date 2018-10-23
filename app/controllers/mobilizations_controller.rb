18216a2e802fcbcaef6e93d55d2a8ac050fe2ef9
Victor Vitalino <vitalino.victor@gmail.com>
2018-10-23 10:40:48 -0300

    mobilizacao

---
+++ app/controllers/mobilizations_controller.rb
@@ -0,0 +1,6 @@
+class MobilizationsController < ApplicationController
+  def index
+  end
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :posts, path: 'postagens'
   resources :actions, path: 'acoes-urbanas'
+  resources :mobilizations, path: 'mobilizacao'
 end
