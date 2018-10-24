bfac2a2b2934e166fc193a6987086a8ac8b0456a
Cleuber <cleubercanamari11@gmail.com>
2018-10-24 15:33:12 -0300

    Finalizacao de acoes urbanas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :posts, path: 'postagens'
-  resources :actions, path: 'acoes-urbanas'
   resources :social_events, path: 'eventos-sociais'
+
+  resources :actions, path: 'acoes-urbanas'
+  get '/acoes_concluidas', to: 'actions#done_action'
+  get 'galeria', to: 'actions#galeria'
 end
